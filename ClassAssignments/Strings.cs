f9fd28d9ad128ca3b14392eb19945ab6fa965a17
aafiya2004 <137600221+aafiya2004@users.noreply.github.com>
2025-04-04 17:45:36 +0530

    Add files via upload

---
+++ ClassAssignments/Strings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    internal class Strings
+    {
+        static void Main()
+        {
+            //1.Write a program in C# to accept a word from the user and display the length of it.
+
+            Console.WriteLine("Enter Name: ");
+            string name=Console.ReadLine();
+            Console.WriteLine("The Length is: "+ name.Length);
+
+
+            //2.Write a program in C# to accept a word from the user and display the reverse of it.
+            Console.WriteLine("Enter Name: ");
+            string n = Console.ReadLine();
+            string reversed = "";
+            for (int i = n.Length-1; i >=0; i--)
+            {
+                   reversed += n[i];
+            }
+            Console.WriteLine(reversed);
+
+            //checking if two strings are equal
+            Console.WriteLine("Enter first name: ");
+            string first = Console.ReadLine();
+            Console.WriteLine("Enter second name: ");
+            string second = Console.ReadLine();
+
+            bool result= strings3.check(first,second);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
---
+++ ClassAssignments/strings3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    internal class strings3
+    {
+        //3.Write a program in C# to accept two words from user and find out if they are same.
+
+        public static bool check(string first, string second)
+        {
+            
+            if (first.Length != second.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < first.Length; i++)
+                {
+                    if (first[i] != second[i])
+                    {
+                        return false;
+                    }
+                }
+                    return true;
+
+                
+            }
+        }
+    }
+}
+
