b841af60767e289960b8fa77078b6edffad00b62
aafiya2004 <137600221+aafiya2004@users.noreply.github.com>
2025-04-07 22:49:05 +0530

    Add files via upload

---
+++ Assignment/CSHARP/Task 9/ITeacherService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.DAO
+{
+    internal interface ITeacherService
+    {
+        int GetOrAddTeacher(int teacherID,string firstName, string lastName, string email);
+    }
+}
