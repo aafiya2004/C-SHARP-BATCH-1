b5fd065e9529829c810311c8db21a7be0b2fcd1c
aafiya2004 <137600221+aafiya2004@users.noreply.github.com>
2025-04-07 22:15:30 +0530

    Add files via upload

---
+++ Assignment/CSHARP/Task 8/IStudentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.DAO
+{
+    internal interface IStudentService
+    {
+        void EnrollStudentWithCourses(Student student, List<int> courseIds, DateTime enrollmentDate);
+  
+        
+    }
+}
