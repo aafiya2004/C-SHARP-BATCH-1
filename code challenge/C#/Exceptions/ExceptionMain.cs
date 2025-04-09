16fd8eeb1687a649f137bf0e2122e7f1c4a26eb9
aafiya2004 <aafiyafarheen.e.2021.cce@ritchennai.edu.in>
2025-04-09 14:28:09 +0530

    Added

---
+++ code challenge/C#/Exceptions/ExceptionMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetPals.Entity;
+
+namespace PetPals.Exceptions
+{
+    internal class ExceptionMain
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Pet pet = new Pet("Rocky", -3, "Labrador");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Invalid age exception" +ex);
+            }
+
+            try
+            {
+                Donation donation = new CashDonation("John Doe", 5, DateTime.Now);
+            }
+            catch (InsufficientFundsException ex)
+            {
+                Console.WriteLine("Insufficient funds exception: " + ex.Message);
+            }
+        }
+    }
+}
