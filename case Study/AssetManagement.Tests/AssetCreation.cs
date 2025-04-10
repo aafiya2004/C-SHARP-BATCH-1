a3913458eda50bc9b82cbc1213ce2a39f37b7180
aafiya2004 <aafiyafarheen.e.2021.cce@ritchennai.edu.in>
2025-04-10 21:45:01 +0530

    Testing completed

---
+++ case Study/AssetManagement.Tests/AssetCreation.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using AssetManagementApp.DAO;
+using AssetManagementApp.Entity;
+using AssetManagementApp.Utility;
+
+namespace AssetManagement.Tests
+{
+    
+
+    namespace AssetManagementApp.Tests
+    {
+        [TestFixture]
+        public class AssetService_Tests
+        {
+            private AssetManagementServiceImpl service;
+
+            [SetUp]
+            public void Setup()
+            {
+                service = new AssetManagementServiceImpl();
+            }
+
+            [Test]
+            public void AddAsset_ShouldReturn_True_WhenInserted()
+            {
+
+                Asset asset = new Asset(
+                    name: "TestAsset",
+                    type: "TestType",
+                    serialNumber: "TT14789",
+                    purchaseDate: DateTime.Now,
+                    location: "TestLocation",
+                    status: "In Use",
+                    ownerId: 1
+                );
+
+
+                bool result = service.AddAsset(asset);
+
+
+                Assert.That(result, Is.True);
+            }
+        }
+    }
+}
---
+++ case Study/AssetManagement.Tests/ExceptionTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssetManagementApp.DAO;
+using NUnit.Framework;
+using AssetManagementApp.Entity;
+using static AssetManagementApp.Exceptions.CustomExceptions;
+
+
+namespace AssetManagement.Tests
+{
+    [TestFixture]
+    public class ExceptionTest
+    {
+        private AssetManagementServiceImpl service;
+
+        [SetUp]
+        public void Setup()
+        {
+            service = new AssetManagementServiceImpl();
+        }
+        [Test]
+        public void DeleteAsset_ShouldThrow_AssetNotFoundException_WhenAssetDoesNotExist()
+        {
+            
+            int assetId = 0; 
+          
+            var ex = Assert.Throws<AssetNotFoundException>(() => service.DeleteAsset(assetId));
+            TestContext.Out.WriteLine("Caught Exception Message: " + ex.Message);
+            Assert.That(ex.Message, Is.EqualTo("Failed to delete asset."));
+
+        }
+
+    }
+}
