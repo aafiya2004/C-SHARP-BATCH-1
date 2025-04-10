da102bae394610e712f0f9f1b24ee65d1563eb59
aafiya2004 <137600221+aafiya2004@users.noreply.github.com>
2025-04-10 21:48:37 +0530

    Add files via upload

---
+++ case Study/Test Cases/MaintenanceTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using AssetManagementApp.DAO;
+using AssetManagementApp.Entity;
+using AssetManagementApp.Utility;
+
+
+namespace AssetManagement.Tests
+{
+        [TestFixture]
+        public class MaintenanceRecordTests
+        {
+            private AssetManagementServiceImpl service;
+
+            [SetUp]
+            public void Setup()
+            {
+                service = new AssetManagementServiceImpl();
+            }
+
+            [Test]
+            public void AddMaintenanceRecord_ShouldReturn_True_WhenInserted()
+            {
+            int assetId = 8; 
+            DateTime maintenanceDate = DateTime.Now;
+            string description = "Test maintenance";
+            double cost = 100.0;
+
+            bool result = service.PerformMaintenance(assetId,maintenanceDate,description,cost);
+                Assert.That(result, Is.True);
+            }
+        }
+    }
---
+++ case Study/Test Cases/ReservationTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssetManagementApp.DAO;
+using NUnit.Framework;
+using AssetManagementApp.Entity;
+
+namespace AssetManagement.Tests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        private AssetManagementServiceImpl service;
+
+    
+
+         [SetUp]
+            public void Setup()
+        {
+            service = new AssetManagementServiceImpl();
+        }
+        [Test]
+        public void ReserveAsset_ShouldReturn_True_WhenInserted()
+        {
+
+            int assetId = 14;
+            int employeeId = 5;
+            DateTime ReservationDate = DateTime.Now;
+            DateTime startDate = DateTime.Now.AddDays(1);
+            DateTime endDate = DateTime.Now.AddDays(7);
+            String status = "Pending";
+            
+            bool result = service.ReserveAsset(assetId,employeeId,ReservationDate,startDate,endDate,status);
+            Assert.That(result, Is.True);
+        }
+    }
+}
+
